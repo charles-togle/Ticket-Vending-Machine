a72008d2431b797599d95a5c3ecd554e9a278d01
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-06 23:10:56 +0800

    added stored value card (in dev)

--- src/TVM/components/Button.tsx
+++ src/TVM/components/Button.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 interface ButtonProps {
   text: string;
-  action: React.MouseEventHandler<HTMLButtonElement>;
+  action?: React.MouseEventHandler<HTMLButtonElement>;
 }
 export default function Button({ text, action }: ButtonProps): ReactNode {
   return <button className = "text-center px-5 py-2" onClick={action}>{text}</button>;
--- src/TVM/pages/StoredValue.tsx
+++ src/TVM/pages/StoredValue.tsx
@@ -1,11 +1,85 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
+import Button from "../components/Button";
+interface CardDetails {
+  value: number;
+  cardID: string;
+  isGoodForEntry: boolean;
+  expiryDate: Date;
+}
 
 export default function StoredValue(): ReactNode {
-  return(
-    <section className="fixed inset-0 grid grid-cols-[40vw_60vw] grid-row-[40vh_60vh] gap-x-10 gap-y-4">
-        <div className="p-4 bg-red-50">Card Value</div>
-        <div className="col-start-2 row-span-2 bg-red-50 p-4">Card Options</div>
-        <div className="p-4 bg-red-50">Card Details</div>
+  const [cardDetails, setCardDetails] = useState<CardDetails>();
+
+
+  //LEARN USE OF CONTEXTS!!!!!
+
+  // const addValue = (val: number) => {
+  //   setCardDetails((prev) => {
+  //     if (!prev)
+  //       return {
+  //         value: val,
+  //         cardID: "",
+  //         isGoodForEntry: false,
+  //         expiryDate: new Date(),
+  //       };
+  //     return {
+  //       ...prev,
+  //       value: prev.value + val,
+  //     };
+  //   });
+  // };
+
+  useEffect(() => {
+    setCardDetails({
+      value: 20,
+      cardID: "123456789098765",
+      isGoodForEntry: 20 > 13,
+      expiryDate: new Date("11 / 24 / 2028"),
+    });
+  }, []);
+  return (
+    <section className="fixed inset-0 grid grid-cols-[40vw_60vw] grid-row-[40vh_60vh] gap-x-10 gap-y-4 p-6 bg-blue-100">
+      <div className="p-4 bg-white">
+        <p className="text-2xl font-bold">Card Value:</p>
+        <p className="text-7xl font-bold mt-10">P{cardDetails?.value}.00</p>
+      </div>
+      <div className="col-start-2 row-span-2 bg-white p-4">
+        <h1 className="text-6xl text-center font-semibold">
+          Select Card Options
+        </h1>
+        <Button text="Add Value"></Button>
+        <Button text="Cancel"></Button>
+      </div>
+      <div className="p-4 bg-white flex flex-col space-y-4 [&>p]:text-l">
+        <p className="text-xl font-bold">Card Details:</p>
+        {cardDetails?.isGoodForEntry && (
+          <p>
+            <strong>Status: </strong>
+            <br />
+            {cardDetails?.isGoodForEntry
+              ? "Good for Entry"
+              : "Insufficient Balance"}
+          </p>
+        )}
+        {cardDetails?.isGoodForEntry && (
+          <p>
+            <strong>Card ID: </strong>
+            <br />
+            {cardDetails?.cardID}
+          </p>
+        )}
+        {cardDetails?.expiryDate && (
+          <p>
+            <strong>Expiry Date: </strong>
+            <br />
+            {cardDetails?.expiryDate.toLocaleString("en-us", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </p>
+        )}
+      </div>
     </section>
-  )
+  );
 }
