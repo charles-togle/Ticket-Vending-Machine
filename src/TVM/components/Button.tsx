d9a62eb60f1ddcc175e1c968b2940a2e21ea2fb0
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-08 10:47:01 +0800

    updated button component to accept additional classes

--- src/TVM/components/Button.tsx
+++ src/TVM/components/Button.tsx
@@ -2,8 +2,20 @@ import { ReactNode } from "react";
 
 interface ButtonProps {
   text: string;
+  className?: string;
   action?: React.MouseEventHandler<HTMLButtonElement>;
 }
-export default function Button({ text, action }: ButtonProps): ReactNode {
-  return <button className = "text-center px-5 py-2" onClick={action}>{text}</button>;
+export default function Button({
+  className,
+  text,
+  action,
+}: ButtonProps): ReactNode {
+  return (
+    <button
+      className={`${className} text-center px-10 py-5 bg-gray-400 text-black font-semibold text-3xl hover:bg-gray-600 hover:text-white`}
+      onClick={action}
+    >
+      {text}
+    </button>
+  );
 }
