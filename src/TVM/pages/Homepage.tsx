89bb0a3464b78186f66af34972a80b9928ad96cd
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-09 12:25:41 +0800

    Implemented getting card number and catching invalid card number in homepage

--- src/TVM/pages/Homepage.tsx
+++ src/TVM/pages/Homepage.tsx
@@ -1,15 +1,75 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import "../global.css";
 import { useNavigate } from "react-router-dom";
+import Alert from "../components/Alert";
+import Prompt from "../components/Prompt";
+import { useCardDetails } from "../hooks/useCardDetails";
+import getCardByID from "../utilities/getCardDetails";
 
 export default function Home(): React.ReactNode {
+  const [isInvalidCard, setIsInvalidCard] = useState<boolean>(false);
+  const [isInputCard, setIsInputCard] = useState<boolean>(false);
+  const [cardNumber, setCardNumber] = useState("");
+  const { setCardDetails } = useCardDetails();
   const navigation = useNavigate();
+  const navigateNextPage = useCallback(
+    (path: string) => {
+      navigation("./" + path);
+    },
+    [navigation]
+  );
 
-  const navigateNextPage = (path: string) => {
-    navigation("./" + path);
-  };
+  const handleStoredValue = useCallback(() => {
+    setIsInputCard(true);
+  }, []);
+
+  const isCardValid = useCallback(
+    async (cardNumber: string): Promise<boolean> => {
+      const cardDetails = await getCardByID(cardNumber);
+      if (cardDetails.cardID !== "00000") {
+        setCardDetails(cardDetails);
+        return true;
+      } else {
+        return false;
+      }
+    },
+    [setCardDetails]
+  );
+
+  const handleCancelPrompt = useCallback(() => {
+    setIsInputCard(false);
+  }, []);
+
+  const handleConfirmPrompt = useCallback(async () => {
+    const isValid = await isCardValid(cardNumber);
+    if (isValid) {
+      navigateNextPage("stored-value");
+    } else {
+      setCardNumber("");
+      setIsInvalidCard(true);
+    }
+  }, [navigateNextPage, isCardValid, cardNumber]);
+
+  const handleConfirmAlert = useCallback(() => {
+    setIsInvalidCard(false);
+  }, []);
+
   return (
     <section className="w-screen h-screen relative">
+      {isInvalidCard && (
+        <Alert
+          text="WARNING: Card not Found"
+          handleConfirm={handleConfirmAlert}
+        ></Alert>
+      )}
+      {isInputCard && (
+        <Prompt
+          promptText="Input your Card Number"
+          cancelAction={handleCancelPrompt}
+          confirmAction={handleConfirmPrompt}
+          setInputValue={setCardNumber}
+        ></Prompt>
+      )}
       <button className="absolute top-5 right-10 w-1/4 h-30 text-center text-2xl font-bold border-10 border-blue-900 hover:bg-blue-100">
         Select English
       </button>
@@ -25,7 +85,7 @@ export default function Home(): React.ReactNode {
           <p className="text-2xl m-5">Or</p>
           <button
             className="text-4xl w-[90%] h-1/4 border-6 border-black hover:bg-blue-100"
-            onClick={() => navigateNextPage("stored-value")}
+            onClick={handleStoredValue}
           >
             Stored Value Card
           </button>
