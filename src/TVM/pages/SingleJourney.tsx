eb0fe19856f928772d3cc388d11110deed17f4ef
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-09 16:22:18 +0800

    SingleJourney UI and price

--- src/TVM/pages/SingleJourney.tsx
+++ src/TVM/pages/SingleJourney.tsx
@@ -1,32 +1,61 @@
-import { ReactNode, useState } from "react";
-
+import { ReactNode, useEffect, useState } from "react";
+import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 export default function SingleJourney(): ReactNode {
+  const navigate = useNavigate()
   const [selectedStation, setSelectedStation] = useState<number>(-1);
-
-  const stationList = [
-    "Santolan",
-    "Katipunan",
-    "Anonas",
-    "Cubao",
-    "Betty Go Belmonte",
-    "Gilmore",
-    "J. Ruiz",
-    "V. Mapa",
-    "Pureza",
-    "Legarda",
+  const [currentStation, setCurrentStation] = useState<number>(-1);
+  const [ticketFair, setTicketFair] = useState<number>(-1);
+  const stationList: string[] = [
     "Recto",
+    "Legarda",
+    "Pureza",
+    "V. Mapa",
+    "J. Ruiz",
+    "Gilmore",
+    "Betty Go",
+    "Cubao",
+    "Anonas",
+    "Katipunan",
+    "Santolan",
+    "Marikina",
+    "Antipolo",
   ];
 
+  const Prices: number[] = [15, 20, 20, 20, 25, 25, 25, 25, 30, 30, 35, 35];
   const stationIndicators = [
     { Stations: "bg-blue-600" },
     { "Selected Station": "bg-red-500" },
     { "Current Station": "bg-yellow-400" },
   ];
+
+  const calculatePrice = (selectedIndex: number) => {
+    const priceIndex = Math.abs(currentStation - selectedIndex);
+    return Prices[priceIndex];
+  };
+
+  const handleChangeStation = (index: number): void => {
+    setSelectedStation(index);
+    setTicketFair(calculatePrice(index-1));
+  };
+
+  useEffect(() => {
+    setCurrentStation(7)
+  },[]);
+
+  const handleCancel = () => {
+    navigate("/")
+  }
+
+  const handleConfirm = () => {
+    console.log("confirm")
+  }
+
   return (
     <div id="single-journey-container" className="flex flex-row">
       <div
         id="stations"
-        className="h-screen w-1/4 p-10 bg-blue-100 flex flex-col justify-center"
+        className="h-screen w-1/3 pl-15 bg-blue-100 flex flex-col justify-center"
       >
         {stationList.map((station, index) => (
           <div key={index}>
@@ -36,34 +65,39 @@ export default function SingleJourney(): ReactNode {
                 "!font-bold text-xl"
               } font-semibold flex items-center`}
             >
-              <p className="w-[60%] py-0">{station}</p>
+              <p className="w-[50%] py-0">{station}</p>
               <button
                 id="selector"
                 className={`${
-                  index === stationList.length - 1 &&
+                  index === currentStation &&
                   "!bg-yellow-400 pointer-events-none"
-                } bg-blue-600 h-[3rem] w-[3rem] rounded-full hover:brightness-50 border-3 border-gray-500
+                } bg-blue-600 h-[2.2rem] w-[2.2rem] rounded-full hover:brightness-50 border-3 border-gray-500
                 ${selectedStation === index && "bg-red-500"}
 
                 `}
-                onClick={() => setSelectedStation(index)}
+                onClick={() => handleChangeStation(index)}
               ></button>
             </div>
             {index !== stationList.length - 1 && (
               <div
                 id="station connector"
-                className=" ml-[calc(60%+(2.5rem/2)/2)] mt-[-0.5em] mb-[-0.5em] h-10 w-7 bg-gray-500"
+                className=" ml-[calc(50%+0.2rem+(2.2rem/2)/2)] mt-[-0.5em] mb-[-0.5em] h-10 w-2.5 bg-gray-500"
               ></div>
             )}
           </div>
         ))}
       </div>
-      <div id="indicators" className="h-1/2 w-1/4 flex flex-col">
-        <h1 className="text-center text-3xl bg-gray-700 text-white p-5"> Please Select a Station</h1>
+      <div id="indicators" className="h-1/2 w-1/3 flex flex-col">
+        <h1 className="text-center text-3xl bg-gray-700 text-white p-10">
+          Please Select a Station
+        </h1>
         {stationIndicators.map((stationType, index) => {
           const [key, value] = Object.entries(stationType)[0];
           return (
-            <div key={index} className="flex flex-row items-center ml-[25%] space-x-3 mt-5">
+            <div
+              key={index}
+              className="flex flex-row items-center ml-[15%] space-x-3 mt-5"
+            >
               <div
                 className={`  h-[3rem] w-[3rem] border-3 border-gray-500 rounded-full ${value}`}
               ></div>
@@ -71,6 +105,26 @@ export default function SingleJourney(): ReactNode {
             </div>
           );
         })}
+      </div>
+      <div id="actions" className="flex flex-col font-semibold text-3xl w-1/3">
+        <p className="w-full pt-[10%] bg-blue-100 text-left">You are Here: </p>
+        <p className="font-bold text-5xl mb-[15%]">
+          {stationList[currentStation]}
+        </p>
+        <p className="w-full pt-[10%] bg-blue-100 text-left">
+          Selected Station:{" "}
+        </p>
+        <p className="font-bold text-5xl mb-[15%]">
+          {stationList[selectedStation]}
+        </p>
+        <p className="w-full pt-[10%] bg-blue-100 text-left">Ticket Fare: </p>
+        <p className="font-bold text-5xl mb-[15%]">
+          {`${ticketFair === -1 ? "" : `P${ticketFair}.00`}`}
+        </p>
+        <div id="buttons" className="flex space-x-10 absolute bottom-0 mb-10">
+          <Button action={handleConfirm} text="Confirm"></Button>
+          <Button action={handleCancel} text="Cancel"></Button>
+        </div>
       </div>
     </div>
   );
