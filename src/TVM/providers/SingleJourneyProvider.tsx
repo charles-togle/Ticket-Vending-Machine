234e58a22d45bc5441fea3e301decd0b0a3d9dee
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-10 23:22:51 +0800

    Create SingleJourneyProvider.tsx

---
+++ src/TVM/providers/SingleJourneyProvider.tsx
@@ -0,0 +1,18 @@
+import { SingleJourneyType, SingleJourneyContext } from "../context/SingleJourneyContext";
+import React, { useState } from "react";
+
+const SingleJourneyProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+
+  const [singleJourneyTicket, setSingleJourneyTicket] = useState<SingleJourneyType>({
+    stationName: "",
+    price: 0
+  });
+
+  return (
+    <SingleJourneyContext.Provider value={{singleJourneyTicket: singleJourneyTicket, setSingleJourneyTicket}}>{children}</SingleJourneyContext.Provider>
+  );
+};
+
+export default SingleJourneyProvider;
