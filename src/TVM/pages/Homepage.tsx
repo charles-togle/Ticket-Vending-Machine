90035fa5e3a707d59807b7dea32fc518a4e53c1c
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-11 15:37:10 +0800

    Homepage now accepts station inputs

--- src/TVM/pages/Homepage.tsx
+++ src/TVM/pages/Homepage.tsx
@@ -5,18 +5,24 @@ import Alert from "../components/Alert";
 import Prompt from "../components/Prompt";
 import { useCardDetails } from "../hooks/useCardDetails";
 import getCardByID from "../utilities/getCardDetails";
+import { stationList } from "../utilities/stationList";
+import { useSingleJourneyTicket } from "../hooks/useSingleJourney";
 
 export default function Home(): React.ReactNode {
   const [isInvalidCard, setIsInvalidCard] = useState<boolean>(false);
   const [isInputCard, setIsInputCard] = useState<boolean>(false);
   const [cardNumber, setCardNumber] = useState("");
+  const [isChooseStation, setIsChooseStation] = useState(false);
+  const [station, setStation] = useState("Recto");
   const { setCardDetails } = useCardDetails();
-  const navigation = useNavigate();
+  const { setSingleJourneyTicket } = useSingleJourneyTicket();
+
+  const navigate = useNavigate();
   const navigateNextPage = useCallback(
     (path: string) => {
-      navigation("./" + path);
+      navigate("./" + path);
     },
-    [navigation]
+    [navigate]
   );
 
   const handleStoredValue = useCallback(() => {
@@ -54,6 +60,19 @@ export default function Home(): React.ReactNode {
     setIsInvalidCard(false);
   }, []);
 
+  const handleConfirmPromptSingle = useCallback(() => {
+    setIsChooseStation(false);
+    if (station || station != null || station === "") {
+      setSingleJourneyTicket((prev) => ({
+        ...prev,
+        stationName: station,
+      }));
+      navigate("/single-journey"); 
+    }
+    console.log(station)
+
+  }, [setSingleJourneyTicket, station, navigate]);
+
   return (
     <section className="w-screen h-screen relative">
       {isInvalidCard && (
@@ -70,6 +89,17 @@ export default function Home(): React.ReactNode {
           setInputValue={setCardNumber}
         ></Prompt>
       )}
+
+      {isChooseStation && (
+        <Prompt
+          promptText="Choose current Station"
+          setInputValue={setStation}
+          cancelAction={() => setIsChooseStation(false)}
+          confirmAction={handleConfirmPromptSingle}
+          isDropdown={true}
+          dropdownContent={stationList}
+        ></Prompt>
+      )}
       <button className="absolute top-5 right-10 w-1/4 h-30 text-center text-2xl font-bold border-10 border-blue-900 hover:bg-blue-100">
         Select English
       </button>
@@ -78,7 +108,7 @@ export default function Home(): React.ReactNode {
           <h1 className="text-5xl mb-20">Purchase Ticket</h1>
           <button
             className="text-4xl w-[90%] h-1/4 border-6 border-black hover:bg-blue-100"
-            onClick={() => navigateNextPage("single-journey")}
+            onClick={()=>setIsChooseStation(true)}
           >
             Single Journey Ticket
           </button>
