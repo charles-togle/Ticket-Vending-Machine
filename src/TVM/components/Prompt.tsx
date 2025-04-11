20a0cf248353fef0099c697a089214b2dfab3f6f
CharlesTogle <149758871+charles-togle@users.noreply.github.com>
2025-04-11 15:36:31 +0800

    prompt dropdown now available

--- src/TVM/components/Prompt.tsx
+++ src/TVM/components/Prompt.tsx
@@ -7,6 +7,8 @@ interface PromptProps {
   cancelAction?: React.MouseEventHandler<HTMLButtonElement>;
   confirmAction?: React.MouseEventHandler<HTMLButtonElement>;
   inputType?: string;
+  dropdownContent?: string[];
+  isDropdown?: boolean;
 }
 
 export default function Prompt({
@@ -16,18 +18,39 @@ export default function Prompt({
   cancelAction,
   confirmAction,
   inputType,
+  dropdownContent,
+  isDropdown = false,
 }: PromptProps): ReactNode {
   return (
-    <div className="w-screen h-screen flex justify-center items-center absolute">
+    <div className="w-screen h-screen flex justify-center items-center absolute top-0 right-0">
       <div className="w-full h-full absolute top-0 bg-black opacity-60 z-2 "></div>
       <div className="aspect-[16/9] w-1/3 relative bg-gray-900 text-white flex flex-col justify-evenly items-center rounded-xl z-3">
         <p className="absolute top-10 font-bold text-3xl">{promptText}</p>
-        <input
-          type={inputType || "text"}
-          className="bg-white h-10 pl-3 text-xl text-black mt-20"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
+        {!isDropdown && (
+          <input
+            type={inputType || "text"}
+            className="bg-white h-10 pl-3 text-xl text-black mt-20"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        )}
+        {isDropdown && (
+          <select
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            className="w-[30%] py-2 mt-15 px-5 bg-white text-black font-semibold"
+          >
+            <option value="" disabled>
+              ---Please Select a Station---
+            </option>
+            {dropdownContent?.map((option, index) => (
+              <option key={index} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        )}
+
         <div className="flex space-x-5 font-semibold">
           <button
             className="border-3 border-red-500 py-2.5 px-5 hover:bg-red-500"
